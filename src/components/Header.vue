9638a866355492627cfd39ff6c811b2ff3de0fdb
cesar-barahona <cesar.barahona@laureate.net>
2020-07-27 15:30:41 -0600

    update

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <b-navbar toggleable="lg" type="dark" variant="info">
+            <b-navbar-brand disabled>Language Learning App</b-navbar-brand>
+            <b-navbar-nav>
+                <b-nav-item disabled light > Correct: {{numCorrect}}/{{numTotal}}</b-nav-item>
+            </b-navbar-nav>
+
+        </b-navbar>
+    </div>
+</template>
+
+<script>
+export default {
+    props:[
+        'numCorrect',
+        'numTotal'
+    ]
+}
+</script>
